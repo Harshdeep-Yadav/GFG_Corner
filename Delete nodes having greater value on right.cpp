d90517b21a8865d1e1255be827bcf48b8f617683
Harshdeep Yadav <harshdeepy5708@gmail.com>
2023-08-29 23:44:38 +0530

    Solvef

---
+++ Delete nodes having greater value on right.cpp
@@ -0,0 +1,27 @@
+Delete nodes having greater value on right
+
+Delete nodes having greater value on right
+class Solution
+{
+    public:
+    Node *compute(Node *head)
+    {
+        if(head==NULL || head->next==NULL)
+            return head;
+        Node*curr=head;
+        Node*tmp=curr;
+        while( curr->next!=NULL){
+            if(curr->next->data>curr->data){
+                tmp=curr;
+                curr->data=curr->next->data;
+                curr->next=curr->next->next;
+                curr=head;
+            }
+            else{
+                curr=curr->next;
+            }
+        }
+        return head;
+    }
+    
+};
