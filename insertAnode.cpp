322afef1e230f3139ec8b67b2e1627a5c57b7106
Harshdeep Yadav <harshdeepy5708@gmail.com>
2023-10-08 23:43:47 +0530

    Solved

---
+++ insertAnode.cpp
@@ -0,0 +1,26 @@
+class Solution{
+  public:
+    // Should return head of the modified linked list
+    Node *sortedInsert(struct Node* head, int data) {
+        
+        if(data<head->data){
+          Node*newNode=new Node(data);
+          newNode->next=head;
+          head=newNode;
+          return head;
+        }
+       
+       Node*tmp=head;
+       Node*prev=NULL;
+       
+       while(tmp!=NULL && tmp->data<=data){
+           prev=tmp;
+           tmp=tmp->next;
+      }
+    //   connecting
+      Node*newNode=new Node(data);
+        prev->next=newNode;
+        newNode->next=tmp;
+        return head;
+    }
+};
