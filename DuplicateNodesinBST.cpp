e6ee3d3489282595ca22718739af1b545aebfa6a
Harshdeep Yadav <harshdeepy5708@gmail.com>
2023-10-14 00:07:00 +0530

    Solved

--- DuplicateNodesinBST.cpp
+++ DuplicateNodesinBST.cpp
@@ -1,25 +1,28 @@
 class Solution
 {
-    public:
-    //Function to find the nodes that are common in both BST. 
-    unordered_map<int,int>mp;
-    void inorder(Node*root,set<int>&s){
-        if(root==NULL){
+public:
+    // Function to find the nodes that are common in both BST.
+    unordered_map<int, int> mp;
+    void inorder(Node *root, set<int> &s)
+    {
+        if (root == NULL)
+        {
             return;
         }
-        inorder(root->left,s);
+        inorder(root->left, s);
         mp[root->data]++;
-        if(mp[root->data]==2){
+        if (mp[root->data] == 2)
+        {
             s.insert(root->data);
         }
-        inorder(root->right,s);
+        inorder(root->right, s);
     }
-    vector <int> findCommon(Node *root1, Node *root2)
+    vector<int> findCommon(Node *root1, Node *root2)
     {
-        set<int>s;
-        inorder(root1,s);
-        inorder(root2,s);
-        vector<int>ans(s.begin(),s.end());
+        set<int> s;
+        inorder(root1, s);
+        inorder(root2, s);
+        vector<int> ans(s.begin(), s.end());
         return ans;
     }
 };
