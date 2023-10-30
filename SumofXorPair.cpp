209afd9c2b5d7bf1a267ae18ef6753281ce028a8
Harshdeep Yadav <harshdeepy5708@gmail.com>
2023-10-30 21:37:42 +0530

    Coded

--- SumofXorPair.cpp
+++ SumofXorPair.cpp
@@ -1,6 +1,6 @@
-// class Solution{ 
+// class Solution{
 //     public:
-        // ```````````````````TLE 1038``````````````````
+// ```````````````````TLE 1038``````````````````
 //     long long int sumXOR(int arr[], int n){
 //     	long long int xorr=0;
 //     	for(int i=0;i<n;i++){
@@ -11,23 +11,27 @@
 //     	return xorr;
 //     }
 // };
-class Solution{
-    public:
-    long long int sumXOR(int arr[], int n){
-    	long long int res=0;
-    	for(int i=0;i<32;i++){
-    	    long long int IndvSum=0;
-    	    long long cnt0=0,cnt1=0;
-    	    for(int j=0;j<n;j++){
-    	        if(arr[j]%2==0)
-    	            cnt0++;
-    	       else
-    	            cnt1++;
-    	        arr[j]/=2;
-    	    }
-    	    IndvSum=cnt1*cnt0*(1<<i);
-    	    res+=IndvSum;
-    	}
-    	return res;
+class Solution
+{
+public:
+    long long int sumXOR(int arr[], int n)
+    {
+        long long int res = 0;
+        for (int i = 0; i < 32; i++)
+        {
+            long long int IndvSum = 0;
+            long long cnt0 = 0, cnt1 = 0;
+            for (int j = 0; j < n; j++)
+            {
+                if (arr[j] % 2 == 0)
+                    cnt0++;
+                else
+                    cnt1++;
+                arr[j] /= 2;
+            }
+            IndvSum = cnt1 * cnt0 * (1 << i);
+            res += IndvSum;
+        }
+        return res;
     }
 };
