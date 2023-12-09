90e9cf0a58d8d504ac22dde71eb6c555fae1268d
Harshdeep Yadav <harshdeepy5708@gmail.com>
2023-12-09 23:49:44 +0530

    Solved

--- TransfoemtoPrime.cpp
+++ TransfoemtoPrime.cpp
@@ -1,31 +1,38 @@
 class Solution
 {
-    public:
-    bool prime(int x){
-        for(int i=2;i*i<=x;i++){
-            if(x%i==0){
+public:
+    bool prime(int x)
+    {
+        for (int i = 2; i * i <= x; i++)
+        {
+            if (x % i == 0)
+            {
                 return false;
             }
         }
         return true;
     }
-    int minNumber(int arr[],int n)
+    int minNumber(int arr[], int n)
     {
-        int sum=0;
-        
-        for(int i=0;i<n;i++){
-            sum+=arr[i];
+        int sum = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            sum += arr[i];
         }
-        int x=sum;
-       if(x==1){
-           return 1;
-       }
-        while(true){
-        if(prime(sum)){
-            break;
+        int x = sum;
+        if (x == 1)
+        {
+            return 1;
         }
-        sum++;
+        while (true)
+        {
+            if (prime(sum))
+            {
+                break;
+            }
+            sum++;
         }
-        return sum-x;
+        return sum - x;
     }
 };
